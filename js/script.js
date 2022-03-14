490fce7a6582180a02e6336fa6f1e2f2565042c8
Wade <wadeya@ad.vzwcorp.com>
2022-03-13 23:51:04 -0500

    guess checker and game over block

--- js/script.js
+++ js/script.js
@@ -1,1 +1,63 @@
 
+ //assigns a random number beween 1 & 1oo 
+ let randomNum = Math.floor(Math.random() * 100) + 1;
+
+ // stores references to the .results paragraphs, will be used to insert values into the paragraphs
+ const guesses = document.querySelector('.guesses');
+ const lastResult = document.querySelector('.lastResult');
+ const hiOrLow = document.querySelector('.hiOrLow');
+ 
+ // stores references to the input and submit button, 
+ const guessSubmit = document.querySelector('.guessSubmit');
+ const guessField = document.querySelector('.guessField');
+ 
+ 
+ 
+ let guessCount = 1;
+ let resetButton;
+ 
+ 
+ checkGuess = ()=> {
+     const userGuess = Number(guessField.value);
+     if (guessCount === 1) {
+         guesses.textContent = 'Previous Guesses:';     
+     }
+     guesses.textContent += userGuess + ' ';
+ 
+     if (userGuess === randomNum)  {
+         lastResult.textContent = 'Awesome, You Got It!';
+         lastResult.style.backgroundColor = 'green'
+         hiOrLow.textContent = '';
+         setGameOver();
+     } else if (guessCount === 10) {
+         lastResult.textContent = `No More Guesses !!!Game Over!!!`;
+         hiOrLow.textContent = '';   
+     }else{
+         lastResult.textContent = 'Wrong Answer, Try Again!';
+         lastResult.style.backgroundColor = 'red';
+         if (userGuess < randomNum) {
+             hiOrLow.textContent = 'Your Guess was too low';
+             
+         }else if (userGuess > randomNum) {
+             hiOrLow.textContent = 'Your Guess was too high';   
+         }
+     }
+ 
+     guessCount ++;
+     guessField.textContent = '';
+     guessField.focus();
+ };
+ 
+ guessSubmit.addEventListener('click', checkGuess);
+ 
+ 
+ setGameOver = () => {
+     guessField.disable = true;
+     guessSubmit.disable = true;
+     resetButton = document.createElement('button');
+     resetButton.textContent = 'Start New Game';
+     document.body.append(resetButton);
+     resetButton.addEventListener('click', resetGame);
+ }
+
+ 
